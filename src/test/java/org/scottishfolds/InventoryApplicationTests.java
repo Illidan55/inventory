80f874cb422cd51cf2a4644e9164135df976c5fd
Illidan5 <tjasany3@gmail.com>
2024-09-15 16:08:44 -0400

    init

---
+++ src/test/java/org/scottishfolds/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scottishfolds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
